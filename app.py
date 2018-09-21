eaac5f3b2e7f22e79f5dfb991756bf5a8df4cbed
j0hanj0han <johan.chapelain@gmail.com>
2018-09-21 10:38:09 +0200

    Add jenkins status + first routes jobs and job details

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask
+from flask_restplus import Api, Resource
+
+from smartrabbit.jenkins_status import jobs
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
-@app.route('/test')
-def test():
-    return 'Hello Test!'
+@api.route('/jobs')
+class Jobs(Resource):
+    def get(self):
+        return jobs
+
+@api.route('/job/<int:job_id>')
+class Job(Resource):
+    def get(self, job_id):
+        return {'job': jobs[job_id]}
+
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
--- smartrabbit/jenkins_status.py
+++ smartrabbit/jenkins_status.py
@@ -1,7 +1,23 @@
+import pprint
+from datetime import datetime
 
-def whoisjohan():
-    from app import johan
-    return johan
+import jenkins
+
+server = jenkins.Jenkins('http://localhost:8080', username='j0hanj0han', password='coconutz')
+user = server.get_whoami()
+version = server.get_version()
+print('Hello %s from Jenkins %s' % (user['fullName'], version))
+
+jobs = server.get_jobs(view_name='POEI')
+
+for job in jobs:
+    print(job['name'] +"    Status:" + job['color'])
 
 
-hello world
+jobs = server.get_jobs()
+print(jobs)
+last_build_number = server.get_job_info('Jenkins_Project_S1')
+print(last_build_number)
+
+# build a parameterized job
+# requires creating and configuring the api-test job to accept 'param1' & 'param2'
